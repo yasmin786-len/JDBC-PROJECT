d603253ace035fbcc15a869b0eec89670a414188
Sayyad Yasmin <syedyasmin2005@gmail.com>
2026-03-22 15:52:29 +0530

    Refactor import statements in hotelManagement.java

--- hotelManagement.java
+++ hotelManagement.java
@@ -2,7 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.Scanner;
+import java.util.*;
 
 public class hotelManagement {
 
